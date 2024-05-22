cece5dfc6c96fa4835fc114fa72ca5127381121f
brunodangelos <91885731+brunodangelos@users.noreply.github.com>
2024-05-22 00:13:19 -0300

    Update zabbix-agent.so.sh

--- zabbix-agent.so.sh
+++ zabbix-agent.so.sh
@@ -7,7 +7,7 @@ detect_distribution_version() {
         . /etc/os-release
         case "$ID" in
             ubuntu)
-                sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \
+                sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \ #rw
                 && sudo dpkg -i zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \
                 && sudo apt update \
                 && sudo apt install zabbix-agent2 zabbix-agent2-plugin-* \
