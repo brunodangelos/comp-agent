7612b099061f9de6739412609ac7b44523e80537
brunodangelos <91885731+brunodangelos@users.noreply.github.com>
2024-05-22 00:28:53 -0300

    Update zabbix-agent.so.sh

--- zabbix-agent.so.sh
+++ zabbix-agent.so.sh
@@ -7,16 +7,16 @@ detect_distribution_version() {
         . /etc/os-release
         case "$ID" in
             ubuntu)
-                sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \ #rw
-                && sudo dpkg -i zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \
-                && sudo apt update \
-                && sudo apt install zabbix-agent2 zabbix-agent2-plugin-* \
-                && sudo sed -i s/Server\=.*/Server=$1/g /etc/zabbix/zabbix_agent2.conf \
-                && sudo sed -i s/ServerActive\=.*/ServerActive=$1/g /etc/zabbix/zabbix_agent2.conf \
-                && sudo sed -i 's/^Hostname=Zabbix server/# Hostname=Zabbix server/g' /etc/zabbix/zabbix_agent2.conf \
-                && sudo sed -i 's/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g' /etc/zabbix/zabbix_agent2.conf \
-                && sudo systemctl enable zabbix-agent2 \
-                && sudo systemctl restart zabbix-agent2
+                sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb
+                sudo dpkg -i zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb
+                sudo apt update
+                sudo apt install zabbix-agent2 zabbix-agent2-plugin-*
+                sudo sed -i s/Server\=.*/Server=$1/g /etc/zabbix/zabbix_agent2.conf
+                sudo sed -i s/ServerActive\=.*/ServerActive=$1/g /etc/zabbix/zabbix_agent2.conf
+                sudo sed -i 's/^Hostname=Zabbix server/# Hostname=Zabbix server/g' /etc/zabbix/zabbix_agent2.conf
+                sudo sed -i 's/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g' /etc/zabbix/zabbix_agent2.conf
+                sudo systemctl enable zabbix-agent2
+                sudo systemctl restart zabbix-agent2
                 ;;
             debian)
                 echo "Distribuição: $ID"
