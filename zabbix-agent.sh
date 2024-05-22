0ca140c7f27ae81b6eed51a6376ba26e8631f651
brunodangelos <91885731+brunodangelos@users.noreply.github.com>
2024-05-22 00:11:39 -0300

    Update zabbix-agent.sh

--- zabbix-agent.sh
+++ zabbix-agent.sh
@@ -1,4 +1,4 @@
-sudo wget wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \
+sudo wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \
 && sudo dpkg -i zabbix-release_6.0-4+ubuntu$(grep "DISTRIB_RELEASE" /etc/lsb-release | cut -d'=' -f2)_all.deb \
 && sudo apt update \
 && sudo apt install zabbix-agent2 zabbix-agent2-plugin-* \
