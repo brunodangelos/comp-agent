bd270295341087b32623a961fe9a02541d5ba911
brunodangelos <91885731+brunodangelos@users.noreply.github.com>
2024-09-23 23:12:56 -0300

    ADD - Agent 2 Debian Zabbix 7

---
+++ zabbix7-agent2-debian.sh
@@ -0,0 +1,11 @@
+sudo wget abbix-release_7.0-2+debian$(cat /etc/debian_version | cut -d. -f1)_all.deb \
+&& sudo dpkg -i zabbix-release_6.0-4+ubuntu$(cat /etc/debian_version | cut -d. -f1)_all.deb \
+&& sudo apt update \
+&& sudo apt install zabbix-agent2 zabbix-agent2-plugin-* \
+&& sudo sed -i s/Server\=.*/Server=$1/g /etc/zabbix/zabbix_agent2.conf \
+&& sudo sed -i s/ServerActive\=.*/ServerActive=$1/g /etc/zabbix/zabbix_agent2.conf \
+&& sudo sed -i 's/^Hostname=Zabbix server/# Hostname=Zabbix server/g' /etc/zabbix/zabbix_agent2.conf \
+&& sudo sed -i 's/^# HostnameItem=system.hostname/HostnameItem=system.hostname/g' /etc/zabbix/zabbix_agent2.conf \
+&& sudo systemctl enable zabbix-agent2 \
+&& sudo systemctl restart zabbix-agent2
+
