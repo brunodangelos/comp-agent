d3f20ab31030a4b0d2d847a98affd30b814e8b37
brunodangelos <91885731+brunodangelos@users.noreply.github.com>
2024-09-23 23:16:14 -0300

    Correção 2

--- zabbix7-agent2-debian.sh
+++ zabbix7-agent2-debian.sh
@@ -1,5 +1,5 @@
 sudo wget zabbix-release_7.0-2+debian$(cat /etc/debian_version | cut -d. -f1)_all.deb \
-&& sudo dpkg -i zabbix-release_6.0-4+ubuntu$(cat /etc/debian_version | cut -d. -f1)_all.deb \
+&& sudo dpkg -i zabbix-release_7.0-2+debian$(cat /etc/debian_version | cut -d. -f1)_all.deb \
 && sudo apt update \
 && sudo apt install zabbix-agent2 zabbix-agent2-plugin-* \
 && sudo sed -i s/Server\=.*/Server=$1/g /etc/zabbix/zabbix_agent2.conf \
